7b835798c4fd63459afbaf9a6cff0b6ba01cbee9
moseskamira <moses.african@gmail.com>
2021-03-18 16:14:22 +0300

    IDE Setup

---
+++ app/src/main/java/com/team295/expandablelistview/MainActivity.java
@@ -0,0 +1,99 @@
+package com.team295.expandablelistview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        expandableListView = findViewById(R.id.expandableListView);
+        expandableListDetail = getData();
+        expandableListTitle = new ArrayList<>(expandableListDetail.keySet());
+
+        expandableListAdapter = new CustomExpandableListAdapter(this, expandableListTitle, expandableListDetail);
+        expandableListView.setAdapter(expandableListAdapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        expandableListTitle.get(groupPosition) + " List Collapsed.",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        expandableListTitle.get(groupPosition)
+                                + " -> "
+                                + expandableListDetail.get(
+                                expandableListTitle.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT
+                ).show();
+                return false;
+            }
+        });
+    }
+
+    public static HashMap<String, List<String>> getData() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<>();
+
+        List<String> cricket = new ArrayList<String>();
+        cricket.add("India");
+        cricket.add("Pakistan");
+        cricket.add("Australia");
+        cricket.add("England");
+        cricket.add("South Africa");
+
+        List<String> football = new ArrayList<String>();
+        football.add("Brazil");
+        football.add("Spain");
+        football.add("Germany");
+        football.add("Netherlands");
+        football.add("Italy");
+
+        List<String> basketball = new ArrayList<String>();
+        basketball.add("United States");
+        basketball.add("Spain");
+        basketball.add("Argentina");
+        basketball.add("France");
+        basketball.add("Russia");
+
+        expandableListDetail.put("CRICKET TEAMS", cricket);
+        expandableListDetail.put("FOOTBALL TEAMS", football);
+        expandableListDetail.put("BASKETBALL TEAMS", basketball);
+        return expandableListDetail;
+    }
+}
